6508d6fc565a24aed9230a908e3a9b70d323cf80
Samet Gecgel <sgrio1453@gmail.com>
2023-10-07 14:21:58 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,29 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    container : {
+      center : true,
+      screens:{
+        lg: '1140px',
+        xl: '1140px',
+        '2xl': '1140px',
+      }
+    },
     extend: {
       fontFamily : {
-        'gemunu': ['Gemunu Libre', 'sans-serif'],
-        'open': ['Open Sans', 'sans-serif']
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif'],
+      },
+
+      colors:{
+        "gega-red": "#BC1A45",
+        "gega-melon": "#FFD369",
+        "gega-grey" : "#DDDDDD",
+        "gega-white" : "#F7F7F7"
+      },
+
+      spacing: {
+        '128' : '32rem'
       }
     },
   },
